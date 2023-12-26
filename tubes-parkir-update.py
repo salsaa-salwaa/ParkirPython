6acfc85a1abbace1cd4917378c8141aa1922c28d
Salsabil Salwa Putri Maharani <72983959+salsaa-salwaa@users.noreply.github.com>
2023-12-26 22:32:06 +0700

    Update tubes-parkir-update.py

--- tubes-parkir-update.py
+++ tubes-parkir-update.py
@@ -1,4 +1,5 @@
 import datetime
+from tabulate import tabulate
 
 kendaraanmasuk = []
 
@@ -9,31 +10,35 @@ def MenuUtama():
         print("2. Keluar Area Parkir")
         print("3. Admin Parkir")
         print("4. Keluar")
-        pilihanmenu = int(input("Pilih menu (1/2/3/4): "))
 
-        if pilihanmenu == 1:
-            MasukParkir()
-        elif pilihanmenu == 2:
-            KeluarParkir()
-        elif pilihanmenu == 3:
-            AdminParkir()
-        elif pilihanmenu == 4:
-            break
-        else:
-            print("Menu tidak valid. Silakan coba kembali")
+        try:
+            pilihanmenu = int(input("Pilih menu (1/2/3/4): "))
+        
+            if pilihanmenu == 1:
+                MasukParkir()
+            elif pilihanmenu == 2:
+                KeluarParkir()
+            elif pilihanmenu == 3:
+                AdminParkir()
+            elif pilihanmenu == 4:
+                break
+            else:
+                print("Menu tidak valid. Silakan coba kembali")
+        except ValueError:
+            print("Input tidak valid. Masukkan angka (1/2/3/4).")
+            continue
     return MenuUtama
 
 def MasukParkir():
-    platnomor = str(input("Masukkan nomor/plat kendaraan (contoh: D1234AF): "))
+    platnomor = str(input("Masukkan nomor/plat kendaraan: "))
     waktumasuk = datetime.datetime.now()
-    kendaraanmasuk.append(
-        {'nomor_plat': platnomor, 
-        'waktu_masuk': waktumasuk})
+    waktukeluar = None
+    kendaraanmasuk.append({'nomor_plat': platnomor, 'waktu_masuk': waktumasuk, 'waktu_keluar':waktukeluar})
     print("Waktu masuk:", waktumasuk)
-    print("Gerbang masuk terbuka. Silakan masuk")
+    print("Silakan masuk")
 
 def KeluarParkir():
-    platnomor = str(input("Masukkan nomor/plat kendaraan (contoh: D1234AF): "))
+    platnomor = str(input("Masukkan nomor/plat kendaraan: "))
 
     try:
         kendaraan = next((k for k in kendaraanmasuk if k["nomor_plat"] == platnomor), None)
@@ -46,6 +51,7 @@ def KeluarParkir():
 
             biaya_parkir = BiayaParkir(durasi_parkir.total_seconds())
             kendaraan['biaya_parkir'] = biaya_parkir
+            kendaraan['waktu_keluar'] = waktukeluar
             print(f'Biaya total parkir: Rp {biaya_parkir}')
             nominal_pembayaran = float(input('Masukkan nominal pembayaran: '))
             while nominal_pembayaran < biaya_parkir:
@@ -54,36 +60,46 @@ def KeluarParkir():
 
             kembalian = nominal_pembayaran - biaya_parkir
             print(f"Kembalian: Rp {kembalian:,.2f}")
-            print('Gerbang keluar terbuka.Terima kasih!')
+            print('Terimakasih!')
         else:
             print("Error: Kendaraan tidak tercatat masuk.")
     except KeyError:
         print("Error: Kendaraan tidak tercatat masuk.")
 
 def InputPin():
-    pin_admin = int(input("Masukkan PIN akses admin (4 digit): "))
-    if pin_admin == 2810:
-        print("PIN benar!")
-        return True
-    else:
-        print("PIN salah")
-        return False
+    max_coba = 3
+    coba = 0
+    while coba < max_coba:
+        pin_admin = int(input("Masukkan PIN akses admin (4 digit): "))
+        if pin_admin == 2810:
+            print("PIN benar!")
+            return True
+        else:
+            print("PIN salah")
+            coba += 1
+            if coba == max_coba:
+              print("Maaf, Anda telah melebihi batas percobaan.")
+              return False
 
 def AdminParkir():
         if InputPin():
-            print("===== MENU ADMIN =====")
-            print("1. Cetak Transaksi Parkir")
-            print("2. Kembali Menu Utama")
-            print("3. Keluar")
-            input_admin = int(input("Pilih menu (1/2/3): "))
-            if input_admin == 1:
-                CetakTransaksiParkir()
-            elif input_admin == 2:
-                pass
-            elif input_admin == 3:
-                exit()
-            else:
-                print("Menu tidak valid. Silakan coba kembali")
+            while True:
+                print("===== MENU ADMIN =====")
+                print("1. Cetak Transaksi Parkir")
+                print("2. Kembali Menu Utama")
+                print("3. Keluar")
+                try:
+                    input_admin = int(input("Pilih menu (1/2/3): "))
+                    if input_admin == 1:
+                        CetakTransaksiParkir()
+                    elif input_admin == 2:
+                        return
+                    elif input_admin == 3:
+                        exit()
+                    else:
+                        print("Menu tidak valid. Silakan coba kembali")
+                except ValueError:
+                    print("Input tidak valid. Masukkan angka (1/2/3).")
         else:
             print("PIN salah, coba lagi!")
 
@@ -92,30 +108,43 @@ def CetakTransaksiParkir():
         print("Tidak ada transaksi parkir")
     else:
         print("\n===== TRANSAKSI PARKIR =====")
-    for transaksi in kendaraanmasuk:
-        print(f"Nomor Plat: {transaksi['nomor_plat']}")
-        print(f"Waktu Masuk: {transaksi['waktu_masuk']}")
+        headers = ["Nomor Plat", "Waktu Masuk", "Waktu Keluar", "Biaya Parkir"]
+        rows = []
         
-        if 'biaya_parkir' in transaksi:
-            print(f"Biaya Parkir: Rp {transaksi['biaya_parkir']}")
-        else:
-            print("Biaya Parkir: Belum dihitung")
-            
-        print("=" * 30)
+        for transaksi in kendaraanmasuk:
+            nomor_plat = transaksi['nomor_plat']
+            waktu_masuk = str(transaksi['waktu_masuk'])
 
-    total_biaya_parkir = sum(BiayaParkir((datetime.datetime.now() - transaksi['waktu_masuk']).total_seconds()) for transaksi in kendaraanmasuk if 'biaya_parkir' in transaksi)
-    print(f"Total Biaya Parkir: Rp {total_biaya_parkir}")
-    
+            if 'waktu_keluar' in transaksi:
+                waktu_keluar = str(transaksi['waktu_keluar'])
+                if 'biaya_parkir' in transaksi:
+                    biaya_parkir = f"Rp {transaksi['biaya_parkir']}"
+                else:
+                    biaya_parkir = "Belum dihitung"
+            else:
+                waktu_keluar = "Kendaraan masih dalam area parkir"
+                biaya_parkir = ""
+
+            rows.append([nomor_plat, waktu_masuk, waktu_keluar, biaya_parkir])
+
+        print(tabulate(rows, headers, tablefmt="grid"))
+                
 def BiayaParkir(waktu_parkir_detik):
     tarif_per_60_detik = 10000
-    pembulatan_waktu_parkir = ((waktu_parkir_detik - 1) // 60 + 1) * 60
+    pembulatan_waktu_parkir = ((waktu_parkir_detik - 1) // 60 + 1) * 60 
     biaya_parkir = (pembulatan_waktu_parkir // 60) * tarif_per_60_detik
 
-    if waktu_parkir_detik > 240:  
+    denda = 0
+
+    if waktu_parkir_detik > 240:
+        biaya_parkir = 40000
         denda = 0.1 * biaya_parkir
-        if waktu_parkir_detik > 360:  
-            denda = 0.25 * biaya_parkir
-        biaya_parkir += denda
+        print("Denda sebesar 10% dari biaya parkir diterapkan.")
+    elif waktu_parkir_detik > 360:  
+        biaya_parkir = 50000
+        denda = 0.25 * biaya_parkir
+        print("Denda sebesar 25% dari biaya parkir diterapkan.")
+    biaya_parkir += denda
 
     return biaya_parkir
 
